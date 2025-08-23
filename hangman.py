5efeb4eabc6f2ddda1b335bdcba02a9f6a54f901
Divyanshu Agarwal <105172534+thedeskguy@users.noreply.github.com>
2025-08-23 10:58:56 +0000

    version 1.0.1 hangman

--- hangman.py
+++ hangman.py
@@ -1,14 +1,99 @@
 import random
+# print("""
+#  _                                             
+# | |                                            
+# | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+# | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+# | | | | (_| | | | | (_| | | | | | | (_| | | | |
+# |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+#                     __/ |                      
+#                    |___/                       
+
+# """)
+
+stages=[
+    '''
+        _______
+     |/      |
+     |      (_)
+     |      \|/
+     |       |
+     |      / \
+     |
+     |___''',
+     '''
+      _______
+     |/      |
+     |      (_)
+     |      \|/
+     |       |
+     |      / 
+     |
+     |___''',
+        '''
+      _______
+     |/      |
+     |      (_)
+     |      \|/
+     |       
+     |       
+     |
+     |___''',
+         '''
+      _______
+     |/      |
+     |      (_)
+     |      \|
+     |       
+     |       
+     |
+     |___''',
+         '''
+      _______
+     |/      |
+     |      (_)
+     |      
+     |       
+     |       
+     |
+     |___''',
+         '''
+      _______
+     |/      |
+     |      
+     |      
+     |       
+     |       
+     |
+     |___'''
+]
 word_list=["batman","spiderman","superman","camel"]
 
 word=random.choice(word_list)
 print(word)
 
-guess=input("Guess a letter of the word: ").lower()
-print(guess)
+placeholder=""
+word_len=len(word)
+placeholder="_" * word_len
+print(placeholder)
+correct_letter=[]
+game_over=False
 
-for char in word:
-    if char==guess:
-        print("right")
-    else:
-        print("wrong")
+while not game_over:
+    guess=input("Guess a letter of the word: ").lower()
+    print(guess)
+
+    display=""
+    for char in word:
+        if char==guess:
+            display+=char
+            correct_letter.append(char)
+        elif char in correct_letter:
+            display+=char
+        else:
+            display+="_"
+    print(display)
+    if "_" not in display:
+        game_over=True
+        print("You win")
+
