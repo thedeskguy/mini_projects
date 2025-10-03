e43bcb90ad9b75fd5fb30b97d264be4f0a4306e7
Divyanshu Agarwal <105172534+thedeskguy@users.noreply.github.com>
2025-10-03 20:02:02 +0000

    quiz_game_commit

--- quiz game/main.py
+++ quiz game/main.py
@@ -1,5 +1,6 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank=[]
 for question in question_data:
@@ -8,4 +9,10 @@ for question in question_data:
     new_question=Question(question_text,question_answer)
     question_bank.append(new_question)
 
-print(question_bank)
+quiz=QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score is {quiz.score}/{quiz.question_number}")
