3aa39d5f1d23a4dd8f59a1bacd5d6540bd2b0434
Divyanshu Agarwal <105172534+thedeskguy@users.noreply.github.com>
2025-10-14 20:09:08 +0000

    crated paddle that moves up and down

--- ponng_game/main.py
+++ ponng_game/main.py
@@ -4,9 +4,28 @@ screen=Screen()
 screen.bgcolor("black")
 screen.setup(800,600)
 screen.title("Pong")
+screen.tracer(0)
 
+paddle_a=Turtle()
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(350,0)
 
+screen.listen()
+def paddle_a_up():
+    new_y=paddle_a.ycor()+20
+    paddle_a.goto(paddle_a.xcor(),new_y)
+def paddle_a_down():
+    new_y=paddle_a.ycor()-20
+    paddle_a.goto(paddle_a.xcor(),new_y)
+screen.onkey(paddle_a_up,"Up")
+screen.onkey(paddle_a_down,"Down")
 
+game_is_on=True
+while game_is_on:
+    screen.update()
 
 
 
