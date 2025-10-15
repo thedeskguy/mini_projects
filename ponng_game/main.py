1bd4ab12cccb85e7bbb7267c29c050c7331b5d4c
Divyanshu Agarwal <105172534+thedeskguy@users.noreply.github.com>
2025-10-15 14:09:10 +0000

    made a ball class with object for the paddle

--- ponng_game/main.py
+++ ponng_game/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle,Screen
 from paddle import Paddle
+from ball import Ball
+import time
 screen=Screen()
 screen.bgcolor("black")
 screen.setup(800,600)
@@ -8,6 +10,7 @@ screen.tracer(0)
 
 paddle_a=Paddle((350,0))
 paddle_b=Paddle((-350,0))
+ball=Ball()
 
 screen.listen()
 screen.onkey(paddle_a.go_up,"Up")
@@ -17,7 +20,9 @@ screen.onkey(paddle_b.go_down,"s")
 
 game_is_on=True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 
