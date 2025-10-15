cb422ac98bc9cf31eb6ec976512e02095e717171
Divyanshu Agarwal <105172534+thedeskguy@users.noreply.github.com>
2025-10-15 14:02:23 +0000

    made paddle class for betterment

--- ponng_game/main.py
+++ ponng_game/main.py
@@ -1,27 +1,19 @@
 from turtle import Turtle,Screen
-
+from paddle import Paddle
 screen=Screen()
 screen.bgcolor("black")
 screen.setup(800,600)
 screen.title("Pong")
 screen.tracer(0)
 
-paddle_a=Turtle()
-paddle_a.shape("square")
-paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=5,stretch_len=1)
-paddle_a.penup()
-paddle_a.goto(350,0)
+paddle_a=Paddle((350,0))
+paddle_b=Paddle((-350,0))
 
 screen.listen()
-def paddle_a_up():
-    new_y=paddle_a.ycor()+20
-    paddle_a.goto(paddle_a.xcor(),new_y)
-def paddle_a_down():
-    new_y=paddle_a.ycor()-20
-    paddle_a.goto(paddle_a.xcor(),new_y)
-screen.onkey(paddle_a_up,"Up")
-screen.onkey(paddle_a_down,"Down")
+screen.onkey(paddle_a.go_up,"Up")
+screen.onkey(paddle_a.go_down,"Down")
+screen.onkey(paddle_b.go_up,"w")
+screen.onkey(paddle_b.go_down,"s")
 
 game_is_on=True
 while game_is_on:
