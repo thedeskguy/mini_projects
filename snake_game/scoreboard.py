c8d653e7f0745c183b22367e2747f262cf0baada
Divyanshu Agarwal <105172534+thedeskguy@users.noreply.github.com>
2025-11-03 19:52:02 +0000

    final snake game

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -5,27 +5,26 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("/Users/divyanshuagarwal/Desktop/python/snake_game/data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.hideturtle()
         self.penup()
         self.goto(0, 260)
         self.update_score()
+
     def update_score(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
-    def increase_score(self):
-        self.score += 1
-        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("/Users/divyanshuagarwal/Desktop/python/snake_game/data.txt",mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
         self.update_score()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
-        
-
-    def reset_score(self):
-        self.score = 0
-
-    def get_score(self):
-        return self.score
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
